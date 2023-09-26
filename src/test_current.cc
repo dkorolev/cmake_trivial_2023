f35fb417ab353c2bd136e5b0cc0bb5e7d6e011aa
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2023-09-26 18:24:36 +0400

    Added a `current` test.

---
+++ src/test_current.cc
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+// NOTE(dkorolev): Works with regular quotes too, but we're using `current` as a CMake-enabled dependency now!
+// #include <bricks/strings/split.h>
+#include <bricks/strings/split.h>
+
+TEST(Current, StringsSplit) {
+  auto const parts = current::strings::Split("foo,bar,baz", ",");
+  ASSERT_EQ(parts.size(), 3u);
+  EXPECT_EQ("foo", parts[0]);
+  EXPECT_EQ("bar", parts[1]);
+  EXPECT_EQ("baz", parts[2]);
+}
